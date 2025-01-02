7f44a9cc06ee43068047bd6baba09d6019222921
Ikit24 <szszattila2@gmail.com>
2025-01-02 14:47:52 +0200

    Added report and rewrote the whole code

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+def sort_on(dict):
+    return dict["num"]
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(len(file_contents.split()))
-
-def count_characters():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(file_contents.split())} words found in the document")
+    print()
     counts_dict = {}
-    for i in list(file_contents):        
-        i = i.lower() 
-        if i not in counts_dict:
-            counts_dict[i] = 0
-        counts_dict[i] += 1
-    for key, value in counts_dict.items():
-        print(key, value)
+    for i in list(file_contents):
+        if i.isalpha():        
+            i = i.lower()
+            if i not in counts_dict:
+                counts_dict[i] = 1
+            else:
+                counts_dict[i] += 1
+
+    char_list = []
+    for char, count in counts_dict.items():
+        char_list.append({"char": char, "num": count})
+    
+    char_list.sort(reverse=True, key=sort_on)
+    
+    
+    for item in char_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
         
 
-count_characters()
+if __name__ == '__main__':
+    main()
